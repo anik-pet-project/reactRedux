7754accdfdd537704ab094cd3cd67674b51f644b
anik-pet-project <pareman166@gmail.com>
2017-08-22 00:07:56 -0700

    added bfs

--- src/api/algo.js
+++ src/api/algo.js
@@ -1671,3 +1671,173 @@ function liftg(binary) {
         };
     };
 }
+
+//SYNC AJAX
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+	return new Promise(function(resolve){
+		fakeAjax(file,resolve);
+	});
+}
+
+// Request all files at once in
+// "parallel" via `getFile(..)`.
+//
+// Render as each one finishes,
+// but only once previous rendering
+// is done.
+["file1","file2","file3"]
+.map(getFile)
+.reduce(
+	function(chain,filePromise){
+		return chain
+			.then(function(){
+				return filePromise;
+			})
+			.then(output);
+	},
+	Promise.resolve() // fulfilled promise to start chain
+)
+.then(function() {
+	output("Complete!");
+});
+
+// BFS
+// script.js
+var data= {
+  "movies":[
+    {
+      "title": "Diner",
+      "cast": ["x","y","z"]
+    },
+    {
+      "title": "Diner1",
+      "cast": ["a","b","z"]
+    },
+    {
+      "title": "Diner2",
+      "cast": ["b","v","z"]
+    },
+    {
+      "title": "Diner3",
+      "cast": ["i","y","z"]
+    }
+  ]
+}
+var graph
+function setUp(){
+  //console.log(data.movies)
+  graph = new Graph()
+  for(var i=0;i<data.movies.length;i++){
+    var movieName = data.movies[i].title
+    var castName = data.movies[i].cast
+    var movieNode = new Node(movieName)
+    graph.addNode(movieNode)
+
+    for(var j=0;j<castName.length;j++){
+      var castNode = graph.getNode(castName[j])
+      if(!castNode){
+        castNode = new Node(castName[j])
+      }
+      graph.addNode(castNode)
+      movieNode.connect(castNode)
+    }
+  }
+
+  var start = graph.setStart('x')
+  var end = graph.setEnd('z')
+  var queue = []
+  start.searched = true
+  queue.push(start)
+
+  while(queue.length>0){
+    var current = queue.shift()
+    //console.log(current.value);
+    if(current === end){
+      console.log('found'+': '+current.value);
+
+    }
+    var edges = current.edges
+    for(var i=0;i<edges.length;i++){
+      if(!edges[i].searched){
+        edges[i].searched = true
+        edges[i].parent = current
+        queue.push(edges[i])
+      }
+    }
+  }
+
+  var path=[]
+console.log(end);
+path.push(end)
+
+var next = end.parent
+
+while(next !== null){
+  path.push(next)
+  next = next.parent
+}
+console.log(path);
+}
+setUp()
+//console.log(graph);
+
+//Node.js
+function Node(val){
+  this.value = val;
+  this.edges=[];
+  this.searched= false;
+  this.parent=null;
+}
+
+Node.prototype.connect=function(n){
+  this.edges.push(n)
+  n.edges.push(this)
+}
+
+//Graph.js
+function Graph(){
+  this.nodes = [];
+  this.edges = {};
+  this.start = null;
+  this.end = null;
+}
+
+Graph.prototype.addNode = function(n){
+  this.nodes.push(n)
+  this.edges[n.value] = n
+}
+
+Graph.prototype.getNode=function(n){
+  return this.edges[n]
+}
+
+Graph.prototype.setStart=function(actor){
+  this.start = this.edges[actor]
+  return this.start
+}
+
+Graph.prototype.setEnd=function(actor){
+  this.end = this.edges[actor]
+  return this.end
+}
